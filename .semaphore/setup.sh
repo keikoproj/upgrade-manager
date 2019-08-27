f6c63c1fa1d4994b2deae69ebf0ec316f3ca0517
kjones16 <kian_jones@intuit.com>
2019-08-27 12:03:24 -0700

    squashed commits

---
+++ .semaphore/setup.sh
@@ -0,0 +1,13 @@
+cache restore $SEMAPHORE_PROJECT_NAME-dep
+
+if [ ! -d '/packages' ]; then
+  sudo mkdir -p /packages
+  wget https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz
+  sudo mv go1.12.7.linux-amd64.tar.gz /packages
+  export os=$(go env GOOS)
+  export arch=$(go env GOARCH)
+  curl -sL https://go.kubebuilder.io/dl/latest/${os}/${arch} -o kubebuilder_master_${os}_${arch}.tar.gz
+  sudo mv kubebuilder_master_${os}_${arch}.tar.gz /packages
+  ls -l /packages
+  cache store $SEMAPHORE_PROJECT_NAME-dep /packages
+fi
