edac023d50b6c5cb4e6dd914572a5bfbe0b335e3
Oleg Atamanenko <oleg.atamanenko@gmail.com>
2020-01-16 15:12:08 -0800

    Use go 1.13 for building

--- .semaphore/setup.sh
+++ .semaphore/setup.sh
@@ -2,8 +2,8 @@ cache restore $SEMAPHORE_PROJECT_NAME-dep
 # checks if the packages are already installed
 if [ ! -d '/packages' ]; then
   sudo mkdir -p /packages
-  wget https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz
-  sudo mv go1.12.7.linux-amd64.tar.gz /packages
+  wget https://dl.google.com/go/go1.13.6.linux-amd64.tar.gz
+  sudo mv go1.13.6.linux-amd64.tar.gz /packages
   export os=$(go env GOOS)
   export arch=$(go env GOARCH)
   curl -sL https://go.kubebuilder.io/dl/latest/${os}/${arch} -o kubebuilder_master_${os}_${arch}.tar.gz
