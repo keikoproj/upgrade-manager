a90ca9973317f1082ed1d02b685fdde0fbc8cd4f
Oleg Atamanenko <oleg.atamanenko@gmail.com>
2020-02-20 11:42:04 -0800

    Upgrade semaphore to go 1.13.8

--- .semaphore/setup.sh
+++ .semaphore/setup.sh
@@ -1,8 +1,8 @@
 cache restore $SEMAPHORE_PROJECT_NAME-dep-$(checksum .semaphore/setup.sh)
 # checks if the packages are already installed
-if [ ! -d '/packages' -o ! -f '/packages/go1.13.6.linux-amd64.tar.gz' ]; then
+if [ ! -d '/packages' -o ! -f '/packages/go1.13.8.linux-amd64.tar.gz' ]; then
   sudo mkdir -p /packages
-  wget -P /packages/ https://dl.google.com/go/go1.13.6.linux-amd64.tar.gz
+  wget -P /packages/ https://dl.google.com/go/go1.13.8.linux-amd64.tar.gz
   export os=$(go env GOOS)
   export arch=$(go env GOARCH)
   curl -sL https://go.kubebuilder.io/dl/latest/${os}/${arch} -o /packages/kubebuilder_master_${os}_${arch}.tar.gz
