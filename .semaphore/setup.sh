1c79187bfe4c747250bee246a49dc6cebc188868
Oleg Atamanenko <oleg.atamanenko@gmail.com>
2020-03-06 14:26:27 -0800

    Fix shadowing of error.

--- .semaphore/setup.sh
+++ .semaphore/setup.sh
@@ -1,11 +1,16 @@
 cache restore $SEMAPHORE_PROJECT_NAME-dep-$(checksum .semaphore/setup.sh)
 # checks if the packages are already installed
-if [ ! -d '/packages' -o ! -f '/packages/go1.13.8.linux-amd64.tar.gz' ]; then
+GO_VERSION=1.13.8
+KUBE_BUILDER_VERSION=2.2.0
+GO_PACKAGE="/packages/go${GO_VERSION}.linux-amd64.tar.gz"
+export os=$(go env GOOS)
+export arch=$(go env GOARCH)
+KUBE_BUILDER_PACKAGE="/packages/kubebuilder_${KUBE_BUILDER_VERSION}_${os}_${arch}.tar.gz"
+
+if [ ! -d '/packages' -o ! -f "${GO_PACKAGE}" -o ! -f "${KUBE_BUILDER_PACKAGE}" ]; then
   sudo mkdir -p /packages
-  wget -P /packages/ https://dl.google.com/go/go1.13.8.linux-amd64.tar.gz
-  export os=$(go env GOOS)
-  export arch=$(go env GOARCH)
-  curl -sL https://go.kubebuilder.io/dl/latest/${os}/${arch} -o /packages/kubebuilder_master_${os}_${arch}.tar.gz
+  curl -sL "https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz" -o "${GO_PACKAGE}"
+  curl -sL "https://go.kubebuilder.io/dl/${KUBE_BUILDER_VERSION}/${os}/${arch}" -o "${KUBE_BUILDER_PACKAGE}"
   ls -l /packages
   cache store $SEMAPHORE_PROJECT_NAME-dep-$(checksum .semaphore/setup.sh) /packages
 fi
