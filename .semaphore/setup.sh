9a85bf88084c5cb2ce3bcd58340aa8eb366480b1
Oleg Atamanenko <oleg.atamanenko@gmail.com>
2020-03-09 09:38:26 -0700

    Use existing go version, simplify maintenance (#66)

--- .semaphore/setup.sh
+++ .semaphore/setup.sh
@@ -1,16 +1,14 @@
 cache restore $SEMAPHORE_PROJECT_NAME-dep-$(checksum .semaphore/setup.sh)
-# checks if the packages are already installed
-GO_VERSION=1.13.8
 KUBE_BUILDER_VERSION=2.3.0
-GO_PACKAGE="/packages/go${GO_VERSION}.linux-amd64.tar.gz"
 export os=$(go env GOOS)
 export arch=$(go env GOARCH)
-KUBE_BUILDER_PACKAGE="/packages/kubebuilder_${KUBE_BUILDER_VERSION}_${os}_${arch}.tar.gz"
-
-if [ ! -d '/packages' -o ! -f "${GO_PACKAGE}" -o ! -f "${KUBE_BUILDER_PACKAGE}" ]; then
+KUBE_BUILDER_PACKAGE="/packages/kubebuilder.tar.gz"
+KUBE_BUILDER_MARKER="/packages/kubebuilder_${KUBE_BUILDER_VERSION}.marker"
+# checks if the packages are already installed
+if [ ! -d '/packages' -o ! -f "${KUBE_BUILDER_MARKER}" ]; then
   sudo mkdir -p /packages
-  curl -sL "https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz" -o "${GO_PACKAGE}"
   curl -sL "https://go.kubebuilder.io/dl/${KUBE_BUILDER_VERSION}/${os}/${arch}" -o "${KUBE_BUILDER_PACKAGE}"
+  touch "${KUBE_BUILDER_MARKER}"
   ls -l /packages
   cache store $SEMAPHORE_PROJECT_NAME-dep-$(checksum .semaphore/setup.sh) /packages
 fi
