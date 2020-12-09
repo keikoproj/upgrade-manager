42ca6db4a0617789c3165b7f35b008ac50a8da6a
Adam Malcontenti-Wilson <amalcontenti-wilson@zendesk.com>
2020-12-10 09:29:57 +1100

    Test node uncordon when postDrain / postDrainWait script fails

--- controllers/script_runner.go
+++ controllers/script_runner.go
@@ -140,11 +140,14 @@ func (r *ScriptRunner) PostWait(instanceID string, nodeName string, ruObj *upgra
 			r.error(ruObj, err, msg)
 			result := errors.Wrap(err, msg)
 
-			r.info(ruObj, "Uncordoning the node %s since it failed to run postDrain Script", "nodeName", nodeName)
-			_, err = r.uncordonNode(nodeName, ruObj)
-			if err != nil {
-				r.error(ruObj, err, "Failed to uncordon", "nodeName", nodeName)
+			if !ruObj.Spec.IgnoreDrainFailures {
+				r.info(ruObj, "Uncordoning the node %s since it failed to run postDrainWait Script", "nodeName", nodeName)
+				_, err = r.uncordonNode(nodeName, ruObj)
+				if err != nil {
+					r.error(ruObj, err, "Failed to uncordon", "nodeName", nodeName)
+				}
 			}
+
 			return result
 		}
 	}
