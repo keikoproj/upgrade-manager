7004418e1d506a2e3e6f73be0673dba6adad1930
Eytan Avisror <eytan_avisror@intuit.com>
2021-01-12 17:53:22 -0800

    Add kubernetes API calls
    Signed-off-by: Eytan Avisror <Eytan_Avisror@hotmail.com>
    Signed-off-by: sbadiger <shreyas_badiger@intuit.com>

--- controllers/providers/kubernetes/events.go
+++ controllers/providers/kubernetes/events.go
@@ -16,4 +16,83 @@ limitations under the License.
 
 package kubernetes
 
-// TODO: Kubernetes Events API calls
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/go-logr/logr"
+	"github.com/keikoproj/upgrade-manager/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+type EventWriter struct {
+	kubernetes.Interface
+	logr.Logger
+}
+
+func NewEventWriter(k kubernetes.Interface, logger logr.Logger) *EventWriter {
+	return &EventWriter{
+		Interface: k,
+		Logger:    logger,
+	}
+}
+
+// EventReason defines the reason of an event
+type EventReason string
+
+// EventLevel defines the level of an event
+type EventLevel string
+
+const (
+	// EventLevelNormal is the level of a normal event
+	EventLevelNormal = "Normal"
+	// EventLevelWarning is the level of a warning event
+	EventLevelWarning = "Warning"
+	// EventReasonRUStarted Rolling Upgrade Started
+	EventReasonRUStarted EventReason = "RollingUpgradeStarted"
+	// EventReasonRUInstanceStarted Rolling Upgrade for Instance has started
+	EventReasonRUInstanceStarted EventReason = "RollingUpgradeInstanceStarted"
+	// EventReasonRUInstanceFinished Rolling Upgrade for Instance has finished
+	EventReasonRUInstanceFinished EventReason = "RollingUpgradeInstanceFinished"
+	// EventReasonRUFinished Rolling Upgrade Finished
+	EventReasonRUFinished EventReason = "RollingUpgradeFinished"
+)
+
+func (w *EventWriter) CreateEvent(rollingUpgrade *v1alpha1.RollingUpgrade, reason EventReason, level string, msgFields map[string]string) {
+	b, _ := json.Marshal(msgFields)
+	msgPayload := string(b)
+	t := metav1.Time{Time: time.Now()}
+	event := &corev1.Event{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: fmt.Sprintf("%v-%v.%v", rollingUpgrade.GetName(), time.Now().Unix(), rand.Int()),
+		},
+		Source: corev1.EventSource{
+			Component: "upgrade-manager",
+		},
+		InvolvedObject: corev1.ObjectReference{
+			Kind:            "RollingUpgrade",
+			Name:            rollingUpgrade.GetName(),
+			Namespace:       rollingUpgrade.GetNamespace(),
+			ResourceVersion: rollingUpgrade.GetResourceVersion(),
+			APIVersion:      v1alpha1.GroupVersion.Version,
+			UID:             rollingUpgrade.GetUID(),
+		},
+		Reason:         string(reason),
+		Message:        msgPayload,
+		Type:           level,
+		Count:          1,
+		FirstTimestamp: t,
+		LastTimestamp:  t,
+	}
+
+	w.V(1).Info("publishing event", "event", event)
+	_, err := w.CoreV1().Events(rollingUpgrade.GetNamespace()).Create(context.Background(), event, metav1.CreateOptions{})
+	if err != nil {
+		w.Error(err, "failed to publish event")
+	}
+}
