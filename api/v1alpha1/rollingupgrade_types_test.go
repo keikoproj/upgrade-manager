347fbac478692e8c0d93fc10329cb93a480db0af
xshao <Sheldon_Shao@intuit.com>
2021-03-01 18:38:11 -0800

    Fix based on suggestion
    Signed-off-by: xshao <Sheldon_Shao@intuit.com>

--- api/v1alpha1/rollingupgrade_types_test.go
+++ api/v1alpha1/rollingupgrade_types_test.go
@@ -10,37 +10,37 @@ func TestNodeTurnsOntoStep(t *testing.T) {
 
 	r := &RollingUpgradeStatus{}
 
-	r.NodeTurnsOntoStep("test-asg", "node-1", NodeRotationKickoff)
+	r.NodeStep("test-asg", "node-1", NodeRotationKickoff)
 
 	g.Expect(r.InProcessingNodes).NotTo(gomega.BeNil())
 	g.Expect(r.Statistics).To(gomega.BeNil())
 
-	r.NodeTurnsOntoStep("test-asg", "node-1", NodeRotationDesiredNodeReady)
+	r.NodeStep("test-asg", "node-1", NodeRotationDesiredNodeReady)
 
 	g.Expect(r.Statistics).NotTo(gomega.BeNil())
 	g.Expect(len(r.Statistics)).To(gomega.Equal(1))
 	g.Expect(r.Statistics[0].StepName).To(gomega.Equal(NodeRotationKickoff))
 
 	//Retry desired_node_ready
-	r.NodeTurnsOntoStep("test-asg", "node-1", NodeRotationDesiredNodeReady)
+	r.NodeStep("test-asg", "node-1", NodeRotationDesiredNodeReady)
 	g.Expect(len(r.Statistics)).To(gomega.Equal(1))
 	g.Expect(r.Statistics[0].StepName).To(gomega.Equal(NodeRotationKickoff))
 
 	//Retry desired_node_ready again
-	r.NodeTurnsOntoStep("test-asg", "node-1", NodeRotationDesiredNodeReady)
+	r.NodeStep("test-asg", "node-1", NodeRotationDesiredNodeReady)
 	g.Expect(len(r.Statistics)).To(gomega.Equal(1))
 	g.Expect(r.Statistics[0].StepName).To(gomega.Equal(NodeRotationKickoff))
 
 	//Completed
-	r.NodeTurnsOntoStep("test-asg", "node-1", NodeRotationCompleted)
+	r.NodeStep("test-asg", "node-1", NodeRotationCompleted)
 	g.Expect(len(r.Statistics)).To(gomega.Equal(3))
 	g.Expect(r.Statistics[1].StepName).To(gomega.Equal(NodeRotationDesiredNodeReady))
 	g.Expect(r.Statistics[2].StepName).To(gomega.Equal(NodeRotationTotal))
 
 	//Second node
-	r.NodeTurnsOntoStep("test-asg", "node-2", NodeRotationKickoff)
+	r.NodeStep("test-asg", "node-2", NodeRotationKickoff)
 	g.Expect(len(r.Statistics)).To(gomega.Equal(3))
 
-	r.NodeTurnsOntoStep("test-asg", "node-2", NodeRotationDesiredNodeReady)
+	r.NodeStep("test-asg", "node-2", NodeRotationDesiredNodeReady)
 	g.Expect(len(r.Statistics)).To(gomega.Equal(3))
 }
