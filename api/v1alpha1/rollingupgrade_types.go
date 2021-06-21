3acc2a66ab9cab16a41505513a87d464cb1e2a58
Sheldon Shao <Sheldon_Shao@intuit.com>
2021-06-21 10:59:11 -0700

    Fix metrics issue in V1 (#260)
    * Fix metrics calculation issue in v1
    
    Signed-off-by: xshao <Sheldon_Shao@intuit.com>
    
    * Expose the rollingUpgrade status as metrics
    
    Signed-off-by: xshao <Sheldon_Shao@intuit.com>
    
    * Fix nil exception
    
    Signed-off-by: xshao <Sheldon_Shao@intuit.com>
    
    * Add unit test
    
    Signed-off-by: xshao <Sheldon_Shao@intuit.com>
    
    * Uppgrade controller-runtime to v0.7.0
    
    Signed-off-by: xshao <Sheldon_Shao@intuit.com>
    
    * Revert "Uppgrade controller-runtime to v0.7.0"
    
    This reverts commit 4996bbf8
    
    Signed-off-by: xshao <Sheldon_Shao@intuit.com>
    
    * Keep the new metrics only.
    
    Signed-off-by: xshao <Sheldon_Shao@intuit.com>
    
    * Keep the new metrics only.
    
    Signed-off-by: xshao <Sheldon_Shao@intuit.com>
    
    * Keep the new metrics only.
    
    Signed-off-by: xshao <Sheldon_Shao@intuit.com>
    Signed-off-by: sbadiger <shreyas_badiger@intuit.com>

--- api/v1alpha1/rollingupgrade_types.go
+++ api/v1alpha1/rollingupgrade_types.go
@@ -78,8 +78,10 @@ type RollingUpgradeStatus struct {
 	LastNodeTerminationTime *metav1.Time              `json:"lastTerminationTime,omitempty"`
 	LastNodeDrainTime       *metav1.Time              `json:"lastDrainTime,omitempty"`
 
-	Statistics     []*RollingUpgradeStatistics `json:"statistics,omitempty"`
-	LastBatchNodes []string                    `json:"lastBatchNodes,omitempty"`
+	Statistics []*RollingUpgradeStatistics `json:"statistics,omitempty"`
+	// For backward compatibility
+	LastBatchNodes []string `json:"lastBatchNodes,omitempty"`
+	//NodeInProcessing map[string]*NodeInProcessing `json:"nodeInProcessing,omitempty"`
 }
 
 // RollingUpgrade Statistics, includes summary(sum/count) from each step
@@ -149,6 +151,7 @@ type NodeInProcessing struct {
 
 // Update last batch nodes
 func (s *RollingUpgradeStatus) UpdateLastBatchNodes(batchNodes map[string]*NodeInProcessing) {
+	//s.NodeInProcessing = batchNodes
 	keys := make([]string, 0, len(batchNodes))
 	for k := range batchNodes {
 		keys = append(keys, k)
