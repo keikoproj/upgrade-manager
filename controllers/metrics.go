22d8e954872e9e42a0293e733bd6cfba3640da8e
sbadiger <shreyas_badiger@intuit.com>
2021-06-23 16:34:09 -0700

    addressing review comments
    Signed-off-by: sbadiger <shreyas_badiger@intuit.com>

--- controllers/metrics.go
+++ controllers/metrics.go
@@ -10,6 +10,7 @@ import (
 
 // Update last batch nodes
 func (s *RollingUpgradeContext) UpdateLastBatchNodes(batchNodes map[string]*v1alpha1.NodeInProcessing) {
+	s.RollingUpgrade.Status.NodeInProcessing = batchNodes
 	keys := make([]string, 0, len(batchNodes))
 	for k := range batchNodes {
 		keys = append(keys, k)
