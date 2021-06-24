1403182f9a497a30ad8f66efc09eff8077af96d8
Sheldon Shao <Sheldon_Shao@intuit.com>
2021-06-24 00:18:28 -0700

    Carry the metrics status in RollingUpgrade CR (#267)
    * Update metrics status at same time
    
    Signed-off-by: xshao <Sheldon_Shao@intuit.com>
    
    * Update metrics status when terminating instance
    
    Signed-off-by: xshao <Sheldon_Shao@intuit.com>
    
    * Add terminated step
    
    Signed-off-by: xshao <Sheldon_Shao@intuit.com>
    
    * Add terminated step
    
    Signed-off-by: xshao <Sheldon_Shao@intuit.com>
    
    * Add terminated step
    
    Signed-off-by: xshao <Sheldon_Shao@intuit.com>
    Signed-off-by: sbadiger <shreyas_badiger@intuit.com>

--- controllers/metrics.go
+++ controllers/metrics.go
@@ -8,6 +8,12 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// Update metrics status UpdateMetricsStatus
+func (s *RollingUpgradeContext) UpdateMetricsStatus(batchNodes map[string]*v1alpha1.NodeInProcessing, nodeSteps map[string][]v1alpha1.NodeStepDuration) {
+	s.UpdateLastBatchNodes(batchNodes)
+	s.UpdateStatistics(nodeSteps)
+}
+
 // Update last batch nodes
 func (s *RollingUpgradeContext) UpdateLastBatchNodes(batchNodes map[string]*v1alpha1.NodeInProcessing) {
 	s.RollingUpgrade.Status.NodeInProcessing = batchNodes
@@ -62,9 +68,8 @@ func (s *RollingUpgradeContext) ToStepDuration(groupName, nodeName string, stepN
 	}
 }
 
-// Node turns onto step
-func (s *RollingUpgradeContext) NodeStep(InProcessingNodes map[string]*v1alpha1.NodeInProcessing,
-	nodeSteps map[string][]v1alpha1.NodeStepDuration, groupName, nodeName string, stepName v1alpha1.RollingUpgradeStep) {
+func (s *RollingUpgradeContext) DoNodeStep(InProcessingNodes map[string]*v1alpha1.NodeInProcessing,
+	nodeSteps map[string][]v1alpha1.NodeStepDuration, groupName, nodeName string, stepName v1alpha1.RollingUpgradeStep, endTime metav1.Time) {
 
 	var inProcessingNode *v1alpha1.NodeInProcessing
 	if n, ok := InProcessingNodes[nodeName]; !ok {
@@ -79,7 +84,7 @@ func (s *RollingUpgradeContext) NodeStep(InProcessingNodes map[string]*v1alpha1.
 		inProcessingNode = n
 	}
 
-	inProcessingNode.StepEndTime = metav1.Now()
+	inProcessingNode.StepEndTime = endTime
 	var duration = inProcessingNode.StepEndTime.Sub(inProcessingNode.StepStartTime.Time)
 	if stepName == v1alpha1.NodeRotationCompleted {
 		//Add overall and remove the node from in-processing map
@@ -96,11 +101,17 @@ func (s *RollingUpgradeContext) NodeStep(InProcessingNodes map[string]*v1alpha1.
 		var newOrder = v1alpha1.NodeRotationStepOrders[stepName]
 		if newOrder > oldOrder { //Make sure the steps running in order
 			stepDuration := s.ToStepDuration(groupName, nodeName, inProcessingNode.StepName, duration)
-			inProcessingNode.StepStartTime = metav1.Now()
+			inProcessingNode.StepStartTime = endTime
 			inProcessingNode.StepName = stepName
 			s.addNodeStepDuration(nodeSteps, nodeName, stepDuration)
 		}
 	}
+}
+
+// Node turns onto step
+func (s *RollingUpgradeContext) NodeStep(InProcessingNodes map[string]*v1alpha1.NodeInProcessing,
+	nodeSteps map[string][]v1alpha1.NodeStepDuration, groupName, nodeName string, stepName v1alpha1.RollingUpgradeStep) {
+	s.DoNodeStep(InProcessingNodes, nodeSteps, groupName, nodeName, stepName, metav1.Now())
 }
 
 func (s *RollingUpgradeContext) addNodeStepDuration(steps map[string][]v1alpha1.NodeStepDuration, nodeName string, nsd v1alpha1.NodeStepDuration) {
