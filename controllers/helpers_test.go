5cc4be2a9b3658c7282e52f7c98c2405038f6639
sbadiger <shreyas_badiger@intuit.com>
2021-08-24 22:41:49 -0700

    Set Instances to StandBy in batches
    Signed-off-by: sbadiger <shreyas_badiger@intuit.com>

--- controllers/helpers_test.go
+++ controllers/helpers_test.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"context"
 	"strings"
 	"sync"
 	"testing"
@@ -348,4 +349,8 @@ func (m *MockEC2) DescribeInstances(*ec2.DescribeInstancesInput) (*ec2.DescribeI
 			&ec2.Reservation{Instances: createEc2Instances()},
 		},
 	}, nil
+}
+
+func (m *MockAutoscalingGroup) EnterStandby(ctx context.Context, params *autoscaling.EnterStandbyInput, optFns ...func(*autoscaling.Options)) (*autoscaling.EnterStandbyOutput, error) {
+	return &autoscaling.EnterStandbyOutput{}, nil
 }
